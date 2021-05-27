d916e6f1b7e5ed3863f22531a17ae244c786ae3e
87vasilevich <87vasilevich@gmail.com>
2021-05-27 21:51:05 +0300

    реализована отправка на email

--- CourseWork_BookShop/MVVM/ViewModel/BasketViewModel.cs
+++ CourseWork_BookShop/MVVM/ViewModel/BasketViewModel.cs
@@ -4,8 +4,11 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace CourseWork_BookShop.MVVM.ViewModel
 {
@@ -26,6 +29,9 @@ namespace CourseWork_BookShop.MVVM.ViewModel
         public string datestart;
         public string dateend;
         public double _finalsum;
+
+        //Переменная для отправки уведомлений на почту
+        public Users temporaryUser;
 
         //Переменные для уведомлений
         public string _ntext;
@@ -51,6 +57,16 @@ namespace CourseWork_BookShop.MVVM.ViewModel
             set
             {
                 dateend = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Users SendEmailToUser
+        {
+            get { return temporaryUser; }
+            set
+            {
+                temporaryUser = value;
                 OnPropertyChanged();
             }
         }
@@ -222,6 +238,7 @@ namespace CourseWork_BookShop.MVVM.ViewModel
 
         public BasketViewModel(MainViewModel mainVM, int _userID)
         {
+            Oplata = "карта";
             //------------------------------------------
             int temp_userID = _userID;
             AllfromBasket = Get_allfromBasket(temp_userID);
@@ -250,93 +267,139 @@ namespace CourseWork_BookShop.MVVM.ViewModel
 
             DeleteFromBasket = new RelayCommand(o => //----------------очистка всей корзины
             {
-                foreach(Basket b in AllfromBasket)
+                MessageBoxResult message;
+                message = MessageBox.Show("Вы уверены?", "Корзина", MessageBoxButton.YesNo);
+                if (message == MessageBoxResult.Yes)
                 {
-                    basket_db.Delete(b.BasketID);
-                }
-                basket_db.Save();
+                    foreach (Basket b in AllfromBasket)
+                    {
+                        basket_db.Delete(b.BasketID);
+                    }
+                    basket_db.Save();
 
-                AllfromBasket = Get_allfromBasket(temp_userID);
-                if (AllfromBasket.Count == 0)
-                {
-                    IsBasketEmpty = "Корзина пуста!";
-                }
+                    AllfromBasket = Get_allfromBasket(temp_userID);
+                    if (AllfromBasket.Count == 0)
+                    {
+                        IsBasketEmpty = "Корзина пуста!";
+                    }
 
-                //Подсчёт общей суммы заказа
-                FinalSum = 0;
+                    //Подсчёт общей суммы заказа
+                    FinalSum = 0;
+
+                    MessageBox.Show("Корзина очищена!", "Корзина");
+                }
+                
             });
 
             AddOrder = new RelayCommand(o => //----------------Оформление заказа
             {
-                //------------------------------------------
-                DateStart = DateTime.Now.ToString(); //Дата оформления
-
-                //Работаем с датой окончания
-                date_end = DateTime.Now;
-                int days = rnd.Next(1, 3);
-                date_end = date_end.AddDays(days);
-                DateEnd = date_end.ToString();
-
-                //Процесс оформления
-                FinalSum = 0;
-                foreach (Basket b in AllfromBasket) 
+                if (AllfromBasket.Count!=0)
                 {
-                    //Получаем данные
-                    BookID = (int)b.Basket_BookID;
-                    BAmount = b.BasketBookAmount.ToString();
-                    AName = b.A_Name;
-                    ASurname = b.A_Surname;
-                    BName = b.B_Name;
-                    OrderTotalSum = (double)b.BasketTotalSum;
+                    MessageBoxResult message;
+                    message = MessageBox.Show("Вы уверены?", "Оформление заказа", MessageBoxButton.YesNo);
+                    if (message == MessageBoxResult.Yes)
+                    {
+                        //------------------------------------------
+                        DateStart = DateTime.Now.ToString(); //Дата оформления
 
-                    //Общая сумма заказа
-                    FinalSum += OrderTotalSum;
+                        //Работаем с датой окончания
+                        date_end = DateTime.Now;
+                        int days = rnd.Next(1, 3);
+                        date_end = date_end.AddDays(days);
+                        DateEnd = date_end.ToString();
 
-                    order_db.Create(new Orders(AName, ASurname, BName, DateStart, DateEnd, temp_userID, BookID, OrderTotalSum, Convert.ToInt32(BAmount)));
-                    order_db.Save();
+                        //Процесс оформления
+                        FinalSum = 0;
+                        foreach (Basket b in AllfromBasket)
+                        {
+                            //Получаем данные
+                            BookID = (int)b.Basket_BookID;
+                            BAmount = b.BasketBookAmount.ToString();
+                            AName = b.A_Name;
+                            ASurname = b.A_Surname;
+                            BName = b.B_Name;
+                            OrderTotalSum = (double)b.BasketTotalSum;
+
+                            //Общая сумма заказа
+                            FinalSum += OrderTotalSum;
+
+                            order_db.Create(new Orders(AName, ASurname, BName, DateStart, DateEnd, temp_userID, BookID, OrderTotalSum, Convert.ToInt32(BAmount)));
+                            order_db.Save();
+                        }
+
+                        //Процесс добавления уведомления
+                        AllfromOrders = Get_allfromOrders(DateStart);
+                        NotifText = "Заказ:\n";
+                        foreach (Orders ord in AllfromOrders)
+                        {
+                            NotifText += $"Название: {ord.B_Name} --- Количество: {ord.OrderBookAmount}\n";
+                        }
+                        NotifText += $"Сумма заказа: {FinalSum}$\n";
+                        NotifText += $"Способ оплаты при получении: {Oplata}.\n";
+                        NotifText += $"Дата оформления заказа: {DateStart}. Дата доставки: {DateEnd}.";
+                        notification_db.Create(new Notifications(_userID, NotifText));
+                        notification_db.Save();
+
+                        #region Отправка на почту
+                        SendEmailToUser = users_db.GetElement(_userID);
+
+                        // отправитель - устанавливаем адрес и отображаемое в письме имя
+                        MailAddress from = new MailAddress("knizhnyymir2021@gmail.com", "Книжный Мир");
+                        // кому отправляем
+                        MailAddress to = new MailAddress(SendEmailToUser.UserEmail);
+                        // создаем объект сообщения
+                        MailMessage m = new MailMessage(from, to);
+                        // тема письма
+                        m.Subject = "Заказ в книжном интернет-магазине";
+                        // текст письма
+                        m.Body = $"<h2>{NotifText}<h2>";
+                        // письмо представляет код html
+                        m.IsBodyHtml = true;
+                        // адрес smtp-сервера и порт, с которого будем отправлять письмо
+                        SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+                        // логин и пароль
+                        smtp.Credentials = new NetworkCredential("knizhnyymir2021@gmail.com", "qwerty228332");
+                        smtp.EnableSsl = true;
+                        smtp.Send(m);
+                        #endregion
+
+                        //очистка корзины
+                        foreach (Basket b in AllfromBasket)
+                        {
+                            basket_db.Delete(b.BasketID);
+                        }
+                        basket_db.Save();
+                        //------------------------------------------
+
+
+                        //------------------------------------------
+                        //Обновление корзины
+                        AllfromBasket = Get_allfromBasket(temp_userID);
+                        if (AllfromBasket.Count == 0)
+                        {
+                            IsBasketEmpty = "Корзина пуста!";
+                        }
+
+                        //Подсчёт общей суммы заказа
+                        FinalSum = 0;
+                        foreach (Basket b in AllfromBasket)
+                        {
+                            //Получаем данные
+                            OrderTotalSum = (double)b.BasketTotalSum;
+
+                            //Общая сумма заказа
+                            FinalSum += OrderTotalSum;
+                        }
+                        //------------------------------------------
+
+
+                        MessageBox.Show("Ваш заказ успешно офорлмен!\nВы можете посмотреть подробную информацию о нём в разделе Мои заказы\nили в письме, оправленном на ваш Email.", "Оформление заказа");
+                    }
                 }
-
-                //Процесс добавления уведомления
-                AllfromOrders = Get_allfromOrders(DateStart);
-                NotifText = "Заказ:\n";
-                foreach(Orders ord in AllfromOrders)
+                else
                 {
-                    NotifText += $"Название: {ord.B_Name} --- Количество: {ord.OrderBookAmount}\n";
+                    MessageBox.Show("Вы ничего не добавили в корзину!", "Оформление заказа");
                 }
-                NotifText += $"Сумма заказа: {FinalSum}$\n";
-                NotifText += $"Способ оплаты при получении: {Oplata}.\n";
-                NotifText += $"Дата оформления заказа: {DateStart}. Дата доставки: {DateEnd}.";
-                notification_db.Create(new Notifications(_userID, NotifText));
-                notification_db.Save();
-
-                //очистка корзины
-                foreach (Basket b in AllfromBasket)
-                {
-                    basket_db.Delete(b.BasketID);
-                }
-                basket_db.Save();
-                //------------------------------------------
-
-
-                //------------------------------------------
-                //Обновление корзины
-                AllfromBasket = Get_allfromBasket(temp_userID);
-                if (AllfromBasket.Count == 0)
-                {
-                    IsBasketEmpty = "Корзина пуста!";
-                }
-
-                //Подсчёт общей суммы заказа
-                FinalSum = 0;
-                foreach (Basket b in AllfromBasket)
-                {
-                    //Получаем данные
-                    OrderTotalSum = (double)b.BasketTotalSum;
-
-                    //Общая сумма заказа
-                    FinalSum += OrderTotalSum;
-                }
-                //------------------------------------------
             });
         }
 
